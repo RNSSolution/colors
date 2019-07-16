ecabe81f9dbcdb1acb8f42b8472960253da52cec
Faisal Naveed <faisal.naveed.akbar@gmail.com>
2019-07-16 10:40:55 +0500

    configure colors with localnet dockers

--- networks/local/colordnode/wrapper.sh
+++ networks/local/colordnode/wrapper.sh
@@ -3,15 +3,15 @@
 ##
 ## Input parameters
 ##
-BINARY=/gaiad/${BINARY:-gaiad}
+BINARY=/colord/${BINARY:-colord}
 ID=${ID:-0}
-LOG=${LOG:-gaiad.log}
+LOG=${LOG:-colord.log}
 
 ##
 ## Assert linux binary
 ##
 if ! [ -f "${BINARY}" ]; then
-	echo "The binary $(basename "${BINARY}") cannot be found. Please add the binary to the shared folder. Please use the BINARY environment variable if the name of the binary is not 'gaiad' E.g.: -e BINARY=gaiad_my_test_version"
+	echo "The binary $(basename "${BINARY}") cannot be found. Please add the binary to the shared folder. Please use the BINARY environment variable if the name of the binary is not 'colord' E.g.: -e BINARY=colord_my_test_version"
 	exit 1
 fi
 BINARY_CHECK="$(file "$BINARY" | grep 'ELF 64-bit LSB executable, x86-64')"
@@ -23,7 +23,7 @@ fi
 ##
 ## Run binary with all parameters
 ##
-export GAIADHOME="/gaiad/node${ID}/gaiad"
+export GAIADHOME="/colord/node${ID}/colord"
 
 if [ -d "`dirname ${GAIADHOME}/${LOG}`" ]; then
   "$BINARY" --home "$GAIADHOME" "$@" | tee "${GAIADHOME}/${LOG}"
@@ -31,5 +31,5 @@ else
   "$BINARY" --home "$GAIADHOME" "$@"
 fi
 
-chmod 777 -R /gaiad
+chmod 777 -R /colord
 
